b569ead7cd231f0c5d5da329dc4bfeda4092ce8d
robertdj20 <robertdj20@gmail.com>
2014-04-24 15:56:20 +0200

    Einde van de dag commi BalloonMode

--- SpinOS/BalloonMode/SearchState.py
+++ SpinOS/BalloonMode/SearchState.py
@@ -1,10 +1,16 @@
 __author__ = 'Robert'
 
+from BalloonMode import BalloonMode
+
 class SearchState:
 
+    def __init__(self):
+        self.colors = []
+        self.currentColor = 0
+
     def doe_stap(self, parameters):
-        #doe een stap
-        return
+        if BalloonMode.alive:
+            self.colors = parameters[0]
 
     def find_balloon(self):
         #vind de ballon
