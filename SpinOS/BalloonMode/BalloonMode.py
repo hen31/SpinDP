84e804d3ee88f2e07aa0837ed532d23a2cf89b6a
robertdj20 <robertdj20@gmail.com>
2014-04-25 09:26:16 +0200

    Import fix

--- SpinOS/BalloonMode/BalloonMode.py
+++ SpinOS/BalloonMode/BalloonMode.py
@@ -1,10 +1,5 @@
 __author__ = 'Robert'
 
-from CardState import CardState
-from FoundState import FoundState
-from MoveState import MoveState
-from SearchState import SearchState
-
 import threading
 
 class BalloonMode:
@@ -16,5 +11,6 @@ class BalloonMode:
         self.thread.start()
 
     def run(self):
+        from CardState import CardState
         state = CardState()
         state.doe_stap()
