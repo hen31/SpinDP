b03021578234264f1536f2d2ac43316a6225fa62
LeviGeertsma <levi_geertsma@hotmail.com>
2014-05-06 11:18:40 +0200

    teerbalmode update

--- SpinOS/TeerbalMode/SearchState.py
+++ SpinOS/TeerbalMode/SearchState.py
@@ -1,8 +1,38 @@
+from SimpleCV import *
+
 __author__ = 'Jeroen'
+
+
+image1 = Image("testTeerbal.png")
+image1.show()
+time.sleep(2)
+image2 = image1.colorDistance(color=Color.BLACK).binarize()
+image2 = image2.erode(2)
+image2 = image2.invert()
+image2.show()
+time.sleep(2)
+
+
+
+
+
+
+
+
+
+
+
+
 
 class SearchState:
 
     def __init__(self):
-        print "hoi"
+        image1.show()
+        time.sleep(2)
+        image2 = image1.colorDistance(color=Color.BLACK).binarize()
+        image2 = image2.erode(2)
+        image2 = image2.invert()
+        image2.show()
+        time.sleep(2)
 
 
