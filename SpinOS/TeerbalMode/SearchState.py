35ca224b2ddc72e4278492f82750fc0645009325
LoadingData <jeroenlemmen@msn.com>
2014-05-06 14:43:16 +0200

    TeerbalMode
    teerbal herkennen

--- SpinOS/TeerbalMode/SearchState.py
+++ SpinOS/TeerbalMode/SearchState.py
@@ -2,37 +2,40 @@ from SimpleCV import *
 
 __author__ = 'Jeroen'
 
-
-image1 = Image("testTeerbal.png")
-image1.show()
-time.sleep(2)
-image2 = image1.colorDistance(color=Color.BLACK).binarize()
-image2 = image2.erode(2)
-image2 = image2.invert()
-image2.show()
-time.sleep(2)
-
-
-
-
-
-
-
-
-
-
-
-
-
 class SearchState:
 
-    def __init__(self):
-        image1.show()
+    def __init__(self, image):
+        self.image = image
+
+    def search_teerbal(self):
+        self.image = Image("teerbal raar.png")
+        self.image.show()
+        time.sleep(0.5)
+        bin_image = self.image.colorDistance(color=Color.BLACK).binarize()
+        bin_image.erode(2)
+        bin_image.show()
         time.sleep(2)
-        image2 = image1.colorDistance(color=Color.BLACK).binarize()
-        image2 = image2.erode(2)
-        image2 = image2.invert()
-        image2.show()
-        time.sleep(2)
+
+        blobs = bin_image.findBlobs()
+        print blobs
+        for b in blobs:
+            index = index +1
+        print index
+
+        blobs = blobs.filter(blobs.area() > 100)
+        print blobs
+
+
+        lager = blobs.below(1)
+        print lager
+        index = 0
+        for l in lager:
+            lager[index].show()
+        index = index+1
+
+        print index
+
+        #blobs.draw()
+
 
 
