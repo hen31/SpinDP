35ac62a28891b877d9389cf0ff128932b393d238
Ruben Vogelvanger <rubie_cool@hotmail.com>
2014-05-06 11:00:11 +0200

    MovementHandler

---
+++ SpinOS/MovementHandler.py
@@ -0,0 +1,18 @@
+__author__ = 'Ruben'
+
+class MovementHandler:
+    def __init__(self):
+        #TODO: create connection
+        pass
+
+    def move(self, x, y, z):
+        pass
+
+    def internal_move(self, x, y, z):
+        pass
+
+    def height_move(self, x, y, z):
+        pass
+
+    def die(self):
+        pass
