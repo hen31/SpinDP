087ba2f14ef1ab4453d0c2ee4ea7c9bf6dbebe61
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2014-05-07 13:31:34 +0200

    fixje

--- SpinOS/ManualMode.py
+++ SpinOS/ManualMode.py
@@ -1,5 +1,5 @@
 import threading
-import Command
+from Command import COMMAND
 
 __author__ = 'Hendrik'
 
@@ -13,17 +13,17 @@ class ManualMode:
 
     def process_command(self, command, parameters):
         self.mutex.acquire()
-        if command == Command.MOVE:
+        if command == COMMAND.MOVE:
             angleMove  = parameters[0]
             forceMove  = parameters[1]
             angleTurn  = parameters[2]
             forceTurn  = parameters[3]
             self.handler.move(forceMove, angleMove, forceTurn, angleTurn)
-        elif command == Command.MOVE_INTERNAL:
+        elif command == COMMAND.MOVE_INTERNAL:
             angle  = parameters[0]
             force  = parameters[1]
             self.handler.internal_move(angle, force)
-        elif command == Command.MOVE_HEIGHT:
+        elif command == COMMAND.MOVE_HEIGHT:
             height = parameters[0]
             self.handler.height_move(height)
         self.mutex.release()
