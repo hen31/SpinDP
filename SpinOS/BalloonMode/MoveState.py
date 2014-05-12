a25d81e1267069b3538c04d036175cc11578101e
robertdj20 <robertdj20@gmail.com>
2014-05-12 11:01:51 +0200

    BalloonMode update met ballon herkenning

--- SpinOS/BalloonMode/MoveState.py
+++ SpinOS/BalloonMode/MoveState.py
@@ -1,14 +1,18 @@
 __author__ = 'Robert'
 
+from BalloonMode import BalloonMode
+
 class MoveState:
 
     def __init__(self):
         pass
 
     def doe_stap(self, parameters):
-        #doe een stap
+        if BalloonMode.alive:
+            self.move_to_balloon(parameters[0], parameters[1])
         return
 
-    def move_to_balloon(self):
+    def move_to_balloon(self, color, blob):
         #loop naar de ballon
+        
         return
