e8a48ef6087ab7795a19301ed6f380bb302859ec
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2014-05-13 11:27:51 +0200

    commitje

--- SpinOS/MovementHandler.py
+++ SpinOS/MovementHandler.py
@@ -10,6 +10,8 @@ __author__ = 'Ruben en Hendrik'
 #Lopen gebeurt op gait manier de poten zijn dan als volgt
 # groep 1: 1,5,3
 # groep 2: 4,2,6
+
+
 # 6 mogelijkheden groep is in de lucht groep staat op de grond
 # groep 1 omhoog aan het bewegen, groep 2 op de grond
 # groep 1 naar voren/achteren aan het bewegen, groep 2 op de grond
@@ -17,8 +19,16 @@ __author__ = 'Ruben en Hendrik'
 # groep 2 omhoog aan het bewegen, groep 1 op de grond
 # groep 2 naar voren/achteren aan het bewegen, groep 1 op de grond
 # groep 2 naar beneden aan het bewegen, groep 1 op de grond
-class MovementHandler:
 
+# het aansturen van het lopen gebeurt in de movement methode, deze loopt op zijn eigen thread doorlopend
+# dit gebeurt om stoteren tijdens het lopen te voorkomen,
+# het laatste commando dat is gegeven wordt door lopend uitgevoerd
+
+# als er een verandering van commando's is worden de waarde's waarmee de berekingen worden uitgevoerd opnieuw berekend
+#
+
+
+class MovementHandler:
 
     def __init__(self):
 
@@ -26,20 +36,45 @@ class MovementHandler:
         self.pwm.setPWMFreq(50)                         # Set frequency to 50 Hz
         self.leg = Leg(0, self.pwm)
         self.leg.set_height(180)
+        self.move_degrees = 0
+        self.move_power = 0
+        self.turn_degrees = 0
+        self.turn_power = 0
+        self.height_setting = 50
+        self.internal_degrees = 0
+        self.internal_power = 0
+        self.variable_mutex = threading.Semaphore(1)
 
     def move(self, degreesMove, powerMove, degreesTurn, powerTurn):
-        pass
+        self.variable_mutex.acquire()
+        self.move_degrees = degreesMove
+        self.move_power = powerMove
+        self.turn_degrees = degreesTurn
+        self.turn_power = powerTurn
+        self.variable_mutex.release()
 
     def move_height(self, height):
-        pass
-
-
+        self.variable_mutex.acquire()
+        self.height_setting = height
+        self.variable_mutex.release()
 
     def move_internal(self, degrees, power):
-        pass
+        self.variable_mutex.acquire()
+        self.internal_degrees = degrees
+        self.internal_power = power
+        self.variable_mutex.release()
 
     def die(self):
         pass
 
     def movement(self):
-        pass
+        while True:
+            self.variable_mutex.acquire()
+            power_internal = self.internal_power
+            degrees_internal = self.internal_degrees
+            power_turn = self.turn_power
+            degrees_turn = self.turn_degrees
+            power_move = self.move_power
+            degrees_move = self.move_degrees
+            height = self.height_setting
+            self.variable_mutex.release()
