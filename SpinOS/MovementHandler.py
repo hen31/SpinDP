c0c54f2c6d002e3362f7a605abd63aefc152104a
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2014-05-13 11:13:05 +0200

    comment movementhandler

--- SpinOS/MovementHandler.py
+++ SpinOS/MovementHandler.py
@@ -2,36 +2,44 @@ import threading
 from Adafruit_PWM_Servo_Driver import PWM
 from Leg import Leg
 
-__author__ = 'Ruben'
-
+__author__ = 'Ruben en Hendrik'
+#Layout van poten van spin
+#       14
+#       25
+#       36
+#Lopen gebeurt op gait manier de poten zijn dan als volgt
+# groep 1: 1,5,3
+# groep 2: 4,2,6
+# 6 mogelijkheden groep is in de lucht groep staat op de grond
+# groep 1 omhoog aan het bewegen, groep 2 op de grond
+# groep 1 naar voren/achteren aan het bewegen, groep 2 op de grond
+# groep 1 naar beneden aan het bewegen, groep 2 op de grond
+# groep 2 omhoog aan het bewegen, groep 1 op de grond
+# groep 2 naar voren/achteren aan het bewegen, groep 1 op de grond
+# groep 2 naar beneden aan het bewegen, groep 1 op de grond
 class MovementHandler:
 
+
     def __init__(self):
-        self.mutexMove = threading.Semaphore(1)
-        self.mutexHeight = threading.Semaphore(1)
-        self.mutexIntenal = threading.Semaphore(1)
+
         self.pwm = PWM(0x40, debug=False)               # PWM for the first servo controller
         self.pwm.setPWMFreq(50)                         # Set frequency to 50 Hz
-        self.leg = Leg(0,self.pwm)
+        self.leg = Leg(0, self.pwm)
         self.leg.set_height(180)
 
     def move(self, degreesMove, powerMove, degreesTurn, powerTurn):
-        self.mutexMove.acquire()
-
-        self.mutexMove.release()
+        pass
 
     def move_height(self, height):
-        self.mutexHeight.acquire()
+        pass
 
-        self.mutexHeight.release()
+
 
     def move_internal(self, degrees, power):
-        self.mutexIntenal.acquire()
-
-        self.mutexIntenal.release()
+        pass
 
     def die(self):
-        self.mutexIntenal.acquire()
-        self.mutexHeight.acquire()
-        self.mutexIntenal.acquire()
+        pass
+
+    def movement(self):
         pass
