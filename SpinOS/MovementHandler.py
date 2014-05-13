33feb7565cfab6339b43d44d1896e187e210ea7f
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2014-05-13 13:59:23 +0200

    interal en move ingebouwd

--- SpinOS/MovementHandler.py
+++ SpinOS/MovementHandler.py
@@ -1,4 +1,5 @@
 import threading
+import math
 from Adafruit_PWM_Servo_Driver import PWM
 from Leg import Leg
 
@@ -30,12 +31,23 @@ __author__ = 'Ruben en Hendrik'
 
 class MovementHandler:
 
+    min_height_degrees = 35
+    max_height_degrees = 110
+
+    min_knee_degrees = 50
+
+    max_internal_degrees = 60
+
+    PWM_FREQ = 50
     def __init__(self):
 
-        self.pwm = PWM(0x40, debug=False)               # PWM for the first servo controller
-        self.pwm.setPWMFreq(50)                         # Set frequency to 50 Hz
+        self.pwm = PWM(0x40)                # PWM for the first servo controller
+        self.pwm.setPWMFreq(MovementHandler.PWM_FREQ)    # Set frequency to 50 Hz
+        self.pwm2 = PWM(0x41)               # PWM for the first servo controller
+        self.pwm2.setPWMFreq(MovementHandler.PWM_FREQ)   # Set frequency to 50 Hz
         self.leg = Leg(0, self.pwm)
         self.leg.set_height(180)
+        self.legs = [Leg(1, self.pwm), Leg(2, self.pwm), Leg(3, self.pwm), Leg(4, self.pwm2), Leg(5, self.pwm2), Leg(6, self.pwm2)]
         self.move_degrees = 0
         self.move_power = 0
         self.turn_degrees = 0
@@ -78,3 +90,83 @@ class MovementHandler:
             degrees_move = self.move_degrees
             height = self.height_setting
             self.variable_mutex.release()
+            if power_move != 0 and power_turn != 0:
+                y = ((power_internal * math.cos(degrees_internal)) + 100) / 2
+                #uitrekenen waardes
+                servo_height = (float(MovementHandler.max_height_degrees - MovementHandler.min_height_degrees) / float(100)) * height
+                servo_angle_calc = ((float(MovementHandler.max_internal_degrees) / float(100)) * y) - (MovementHandler.max_internal_degrees / 2)
+
+                #voorste servo 1 en 4
+                servo_front_height = servo_height
+                servo_front_height = servo_front_height + servo_angle_calc
+
+                #midelste servo 2 en 5
+                servo_middle_height = servo_height
+
+                #achterste servo 3 en 6
+                servo_back_height = servo_height
+                servo_back_height = servo_back_height - servo_angle_calc
+
+
+
+
+
+
+
+
+
+
+
+
+
+                #servo voor knee 1 en 4
+                servo_front_knee = MovementHandler.min_knee_degrees + (servo_front_height - MovementHandler.min_height_degrees)
+
+                #servo midelste knee 2 en 5
+                servo_middle_knee = MovementHandler.min_knee_degrees + (servo_middle_height - MovementHandler.min_height_degrees)
+
+                #achterste servo 3 en 6
+                servo_back_knee = MovementHandler.min_knee_degrees + (servo_back_height - MovementHandler.min_height_degrees)
+
+
+
+
+            else:
+
+                y = ((power_internal * math.cos(degrees_internal)) + 100) / 2
+                x = ((power_internal * math.sin(degrees_internal)) + 100) / 2
+                #uitrekenen waardes
+                servo_height = (float(MovementHandler.max_height_degrees - MovementHandler.min_height_degrees) / float(100)) * height
+                servo_angle_calc = ((float(MovementHandler.max_internal_degrees) / float(100)) * y) - (MovementHandler.max_internal_degrees / 2)
+                servo_bank_calc = ((float(MovementHandler.max_internal_degrees) / float(100)) * x) - (MovementHandler.max_internal_degrees / 2)
+
+
+                #voorste servo
+                servo_1 = servo_height
+                servo_1 = servo_front_height + servo_angle_calc
+
+                servo_4 = servo_height
+                servo_4 = servo_front_height + servo_angle_calc
+
+                #midelste servo
+                servo_2 = servo_height
+                servo_5 = servo_height
+
+                #achterste servo
+                servo_3 = servo_height
+                servo_3 = servo_back_height - servo_angle_calc
+
+                servo_6 = servo_height
+                servo_6 = servo_back_height - servo_angle_calc
+
+
+                #left
+                servo_1 += servo_bank_calc #front
+                servo_2 += servo_bank_calc #middle
+                servo_3 += servo_bank_calc #back
+
+                #right
+                servo_4 -= servo_bank_calc #front
+                servo_5 -= servo_bank_calc #middle
+                servo_6 -= servo_bank_calc #back
+
